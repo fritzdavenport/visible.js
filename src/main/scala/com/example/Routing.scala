c9912639c109c2bfaa35c310d3ac78c27afd4281
Fritz Davenport <Fritz.Davenport@dealertrack.com>
2015-11-25 18:32:36 -0500

    renamed files, changed html, added maven build plugin

--- src/main/scala/com/example/Routing.scala
+++ src/main/scala/com/example/Routing.scala
@@ -11,7 +11,7 @@ class RoutingActor extends Actor with StaticRoute {
 
 trait StaticRoute extends HttpService {
   val staticRoute = {
-    path("") {
+    path("visible") {
       getFromResource("client/normal.html")
     } ~ {
       getFromResourceDirectory("client")
